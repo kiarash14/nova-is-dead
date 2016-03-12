ad6f76a3ff462012aa9b7031c983b289b94af06d
mamadiraner <mamali.azim@gmail.com>
2016-03-11 23:43:30 -0800

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (ghost)",-- Put everything you like :)
+		"^[!/](ghost)"-- Put everything you like :)
     }, 
   run = run
 }
